e2ee31b79f0e94c6b9ee88a83f37154ebe3043b2
Willie Hung <sungjiehung@gmail.com>
2021-07-20 22:12:15 +0800

    Update SQL_syntax.sql

--- Practice/SQL_syntax.sql
+++ Practice/SQL_syntax.sql
@@ -40,3 +40,44 @@ SELECT * FROM Friends WHERE First_Name LIKE '_en';
   | Jen        | Peters     | Sweetpea  |  
   +------------+------------+-----------+
   2 rows in set (0.03 sec)
+  
+=============================================================
+  
+/* Create DataBase */
+CREATE DATABASE database_name;
+CREATE DATABASE foo;
+  
+/* Create Table */
+  
+CREATE TABLE table_name (
+  column_name1 data_type,
+  column_name2 data_type,
+  column_name3 data_type,
+  ···
+);
+  
+CREATE TABLE customers (
+  C_Id INT,
+  Name varchar(50),
+  Address varchar(255),
+  Phone varchar(20)
+);
+
+/* Alter Table (Add Column) */
+
+ALTER TABLE table_name ADD column_name datatype;
+
+ALTER TABLE customers ADD Discount VARCHAR(10);
+
+/* (Alter Column Type) */
+
+ALTER TABLE table_name ALTER COLUMN column_name datatype;
+
+ALTER TABLE customers ALTER COLUMN Discount DECIMAL(18, 2);
+
+/* (Drop Column) */
+
+ALTER TABLE table_name DROP COLUMN column_name;
+
+ALTER TABLE customers DROP COLUMN Discount;
+  
